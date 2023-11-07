84e582e042b3c25c37167ba98a3f83e9fa4a5699
LukeMcCann <developer-lukemccann@outlook.com>
2023-11-07 21:06:05 +0000

    Render a basic Window

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,42 @@
 
 int main(int argc, char *argv[]) {
 
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		std::cout << "SDL Init Failed!" << std::endl;
 		return 1;
 	}
 
-	std::cout << "SDL Init succeeded!" << std::endl;
+	SDL_Window *window = SDL_CreateWindow(
+		"Particle Swarm",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT,
+		SDL_WINDOW_SHOWN
+	);
 
+	if (window == NULL) {
+		SDL_Quit();
+		std::cout << "Could not create window: \n" << SDL_GetError() << std::endl;
+		return 2;
+	}
+
+	SDL_Event event;
+	bool quit = false;
+
+	while (!quit) {
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
