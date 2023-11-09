c564d187101bf06ac3041fcea1b2cf736781c9ba
LukeMcCann <developer-lukemccann@outlook.com>
2023-11-09 21:26:18 +0000

    Playing with variables

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -6,17 +6,50 @@
 
 namespace vtech {
 	Particle::Particle(): x(0), y(0) {
-		direction = (2 * M_PI * rand()/RAND_MAX);
-		velocity = (universalConstant * rand())/RAND_MAX;
+		init();
 	}
 
 	Particle::~Particle() {}
 
+	void Particle::init() {
+		x = 0;
+	  y = 0;
+
+		direction = -(2 * M_PI * rand()/RAND_MAX);
+
+		velocity = (expansionSpeed * rand())/RAND_MAX;
+		velocity *= velocity;
+
+		startTime = std::chrono::steady_clock::now();
+	}
+
 	void Particle::update(int interval) {
+		std::chrono::steady_clock::time_point currentTime = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsedTime = std::chrono::duration_cast<std::chrono::duration<double>>(
+			currentTime - startTime
+		);
+
+		if (rand() / RAND_MAX > 0.5) {
+			direction += interval * 0.0004;
+		} else {
+			direction -= interval * 0.0004;
+		}
+
+    if (elapsedTime.count() >= 3.0) {
+      expansionSpeed = 0.023;
+    }
+
+		velocity = (expansionSpeed * rand()) / RAND_MAX;
+		velocity *= velocity;
+
 		double velocityX = velocity * cos(direction);
 		double velocityY = velocity * sin(direction);
 
 		x += velocityX * interval;
 		y += velocityY * interval;
+
+		if (x <= -1.0 || x >= 1.0 || y <= -1.0 || y >= 1.0) {
+      init();
+    }
 	}
 };
--- src/Particle.h
+++ src/Particle.h
@@ -1,9 +1,12 @@
 #ifndef PARTICLE_H
 #define PARTICLE_H
 
+#include <chrono>
+
 namespace vtech {
 	class Particle {
 		public:
+			std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
 			// Ideally this would be private
 			// but in simulations and games
 			// we need to make concessions for efficiency
@@ -12,14 +15,17 @@ namespace vtech {
 			double x;
 			double y;
 
-			const double universalConstant = 0.0001;
+			double expansionSpeed = 0.0002;
 			double direction;
 			double velocity;
+
 
 			Particle();
 			virtual ~Particle();
 
 			void update(int interval);
+		private:
+			void init();
 	};
 };
 
